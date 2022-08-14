6d96401e6da558fbe47bbb07a512afbe01a93279
Gregorio Vargas <gregoriovargasmarrero@gmail.com>
2022-08-14 19:46:24 -0400

    desktop version at 1440 finish

--- src/script.js
+++ src/script.js
@@ -6,3 +6,24 @@ let toggle = true;
 menuElement.addEventListener("click", () => {
     menuContainerElement.classList.toggle("show");
 });
+
+
+
+
+
+const linksElements = document.getElementsByClassName("animated_link");
+
+
+
+const toggleUnderline  = (element) => {
+    element.children[0].classList.toggle("change_opacity");
+}
+
+for (let i = 0; i < linksElements.length; i++) {
+    const element = linksElements[i];
+    element.addEventListener("mouseover", () => toggleUnderline(element));
+    element.addEventListener("mouseleave", () => toggleUnderline(element));
+}
+
+
+
